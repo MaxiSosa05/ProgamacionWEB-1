db93a6a2a23ca796f6f5339ae348e74a246c3f89
Maxi 243 <florymartu15@gmail.com>
2025-06-07 14:57:26 -0300

    Agregue zoom  a portadas de libros

--- script.js
+++ script.js
@@ -5,21 +5,36 @@
 var  mensaje = "";
 
 if (horaActual > 7 && horaActual < 13){
-    mensaje = "Buenos dias."
+    mensaje = "Buenos dias 😊📚."
 }
 else if ( horaActual > 13 && horaActual < 19){
-    mensaje = "Buenas tardes."
+    mensaje = "Buenas tardes 😊📚."
 }
 else{
-    mensaje = "Buenas noches."
+    mensaje = "Buenas noches 😊📚."
 }
 
 function saludo() {
     let nombre = prompt("¿Cómo te llamás?");
     if (nombre) {
-      alert(`¡Hola! ${nombre} ${mensaje}`);
+      alert(`¡Hola ${nombre}! ${mensaje}`);
     } else {
       alert("¡Hola! " + mensaje);
     }
   }
 
+//zoom para las imagenes
+
+const imagenes = document.querySelectorAll('.zoom-libro');
+
+imagenes.forEach((img) => {
+  
+  img.addEventListener('mouseenter', () => {
+    img.classList.add('zoom-activo');
+  });
+
+  
+  img.addEventListener('mouseleave', () => {
+    img.classList.remove('zoom-activo');
+  });
+});
