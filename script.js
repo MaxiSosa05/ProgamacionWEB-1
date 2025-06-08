8303fbd57b4b88aa8848b8a11f9dc9b5267181fd
TrianaClavero <trianaclavero13@gmail.com>
2025-06-08 00:27:34 -0300

    Agrega funcionalidad para redirigir a la página de detalles del libro al hacer clic en el botón "comprar"

--- script.js
+++ script.js
@@ -38,3 +38,30 @@ imagenes.forEach((img) => {
     img.classList.remove('zoom-activo');
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonComprar = document.getElementsByClassName('comprar');
+
+  for (let btn of buttonComprar){
+    btn.addEventListener('click', function(event){
+      const parent = event.target.closest('.section_1_div_cont')
+      const img = parent.querySelector('img');
+      const title = parent.querySelector('h3');
+      const author = parent.querySelector('.h3_autor');
+
+      console.log(`Img (src): ${img}`);
+      console.log(`Titulo: ${title}`);
+      console.log(`Autor: ${author}`);
+
+      const data = {
+        src: img.getAttribute('src'),
+        title: title.textContent,
+        author: author.textContent
+      }
+      sessionStorage.setItem('detalle', JSON.stringify(data));
+      window.location.href = 'libro-especif.html'
+     
+    })
+
+  }
+})
