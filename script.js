063ee9ee016de57a6a2cc71cd8e4f4c64f9224ee
MaxiSosa05 <maxiososa05@gmail.com>
2025-12-18 23:56:03 -0300

    boton saludo - eliminado

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 
- var fechaYHoraActual = new Date();
- var horaActual = fechaYHoraActual.getHours();
- 
-var  mensaje = "";
-
-if (horaActual > 7 && horaActual < 13){
-    mensaje = "Buenos dias 😊📚."
-}
-else if ( horaActual > 13 && horaActual < 19){
-    mensaje = "Buenas tardes 😊📚."
-}
-else{
-    mensaje = "Buenas noches 😊📚."
-}
-
-function saludo() {
-    let nombre = prompt("¿Cómo te llamás?");
-    if (nombre) {
-      alert(`¡Hola ${nombre}! ${mensaje}`);
-    } else {
-      alert("¡Hola! " + mensaje);
-    }
-  }
 
 //zoom para las imagenes
 
